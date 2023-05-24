a190fe117746b34a54fd6c59db00f3ad659b69d6
rubyyareli <47041553+rubyyareli@users.noreply.github.com>
2023-05-24 00:30:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const app = express();
 app.set('port', process.env.PORT || 9000);
 
 const dbOptions = {
-    host: 'srv1026.hstgr.io',
-    port: 3306,
-    user: 'u337397135_rubyyareli',
-    password: ';NYZNKYw7',
-    database: 'u337397135_chococandy'
+    host: 'containers-us-west-96.railway.app',
+    port: 5551,
+    user: 'root',
+    password: 'fLJquorFLE6Y9GpUVzoR',
+    database: 'railway'
 }
 
 //*****\\middlewares//*****\\
