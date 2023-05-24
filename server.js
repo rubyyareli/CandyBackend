e138026c0e79aad68e50c484d27f6b3617fba5ae
rubyyareli <47041553+rubyyareli@users.noreply.github.com>
2023-05-24 00:57:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const app = express();
 app.set('port', process.env.PORT || 9000);
 
 const dbOptions = {
-    host: 'srv1026.hstgr.io', //
-    port: 5000,
+    host: '31.170.161.103', //
+    port: 3306,
     user: 'u337397135_rubyyareli', //
     password: 'n44jWY]8pE', //nueva contrase;a
     database: 'u337397135_chococandy' //
