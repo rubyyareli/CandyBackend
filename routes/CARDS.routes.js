93e3bf5347fb5a91bae5c8280d4811643445c937
rubyyareli <47041553+rubyyareli@users.noreply.github.com>
2023-05-24 03:00:15 -0600

    Update CARDS.routes.js

--- routes/CARDS.routes.js
+++ routes/CARDS.routes.js
@@ -5,7 +5,7 @@ const routesC = express.Router();
 routesC.get('/',(req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('SELECT * FROM CARDS', (err, rows)=>{
+        conn.query('SELECT * FROM cards', (err, rows)=>{
             if(err) return res.send(err)
             res.json(rows)
         })
@@ -15,7 +15,7 @@ routesC.get('/',(req, res)=>{
 routesC.get('/:id',(req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('SELECT * FROM CARDS WHERE FK_USER_ID = ?', [req.params.id], (err, rows)=>{
+        conn.query('SELECT * FROM cards WHERE FK_USER_ID = ?', [req.params.id], (err, rows)=>{
             if(err) return res.send(err)
             res.json(rows)
         })
@@ -25,7 +25,7 @@ routesC.get('/:id',(req, res)=>{
 routesC.post('/', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('INSERT INTO CARDS set ?', [req.body], (err, rows)=>{
+        conn.query('INSERT INTO cards set ?', [req.body], (err, rows)=>{
             if(err) return res.send(err)
 
             res.send('tarjeta agregada!')
@@ -36,7 +36,7 @@ routesC.post('/', (req, res)=>{
 routesC.delete('/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('DELETE FROM CARDS WHERE FK_USER_ID = ?', [req.params.id], (err, rows)=>{
+        conn.query('DELETE FROM cards WHERE FK_USER_ID = ?', [req.params.id], (err, rows)=>{
             if(err) return res.send(err)
 
             res.send('Se elimino la tarjeta')
@@ -47,7 +47,7 @@ routesC.delete('/:id', (req, res)=>{
 routesC.put('/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('UPDATE CARDS set ? WHERE FK_USER_ID = ?', [req.body, req.params.id], (err, rows)=>{
+        conn.query('UPDATE cards set ? WHERE FK_USER_ID = ?', [req.body, req.params.id], (err, rows)=>{
             if(err) return res.send(err)
 
             res.send('la tarjeta se actualizo!')
